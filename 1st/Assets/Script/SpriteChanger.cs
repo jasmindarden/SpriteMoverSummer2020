95577bcebdf3c99f2e2b6d8af6ee37c702470739
Jasmin Darden <jasmindarden@icloud.com>
2020-05-22 12:16:32 -0700

    Project 1 Milestone 1

---
+++ 1st/Assets/Script/SpriteChanger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteChanger : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public Color color;
+    
+    void Awake()
+    {
+
+    }
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        spriteRenderer.color = color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //spriteRenderer.color = color;
+    }
+
+    /// <summary>
+    /// Change the color of the sprite to the color input
+    /// </summary>
+    
+    public void SampleMethod(Color newColor)
+    {
+        spriteRenderer.color = newColor;
+    }
+
+
+}
---
+++ 1st/Assets/Script/SpriteMover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class SpriteMover : MonoBehaviour
+{
+    public float moveSpeed = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+    }
+}
