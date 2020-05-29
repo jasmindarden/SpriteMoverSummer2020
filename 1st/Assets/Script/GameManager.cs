0d991a9c77e00ea84937fa838beb28ab8ffa5047
Jasmin Darden <jasmindarden@icloud.com>
2020-05-29 14:02:30 -0700

    Project 1 Sprite Mover

---
+++ 1st/Assets/Script/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject objectToDeactivate;
+    public SpriteMover componentToDisable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    //Exits the game when you press escape
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+      //While the P key pressed, it disables the sprite and no movements can be made. Press P again to unable it
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            componentToDisable.enabled = !componentToDisable.enabled;
+        }
+
+        //While the Q key is held down, it disables the sprite and makes it disappear
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            objectToDeactivate.SetActive(!objectToDeactivate.activeInHierarchy);
+        }
+    }
+}
--- 1st/Assets/Script/SpriteMover.cs
+++ 1st/Assets/Script/SpriteMover.cs
@@ -8,6 +8,7 @@ using UnityEngine;
 public class SpriteMover : MonoBehaviour
 {
     public float moveSpeed = 0.1f;
+    public float shiftSpeed = 3f;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +18,41 @@ public class SpriteMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //It will move the Beachball Sprite right constantly to set speed (1 unit)
-        transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //It will move the beachball sprite right when we hold the right key to set speed (1 unit)
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            //It will move the beachball sprite when we hold the up key to set speed (1 unit)
+            transform.position += Vector3.up * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //It will move the beachball sprite left when we hold the left key to set speed (1 unit)
+            transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            //It will move the beachball sprite down when we hold the down key to set speed (1 unit)
+            transform.position += Vector3.down * moveSpeed * Time.deltaTime;
+        }
+
+        //Moves the sprite back to the origin (0,0)
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+        }
+
+        //Moves sprite faster when Shift is held down
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            moveSpeed = shiftSpeed;
+        }
+        else 
+        {
+                moveSpeed = 1f;
+        }
     }
 }
